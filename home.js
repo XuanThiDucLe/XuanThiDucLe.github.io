40f420707df3a07b7130cb969cff4198c25a6241
Xuan <xuan@Heidis-Air-2.fritz.box>
2019-05-29 16:21:37 +0200

    j

--- home.js
+++ home.js
@@ -1,7 +1,28 @@
-console.log("hello world")
+console.log("hello world");
 
-var proj1=document.getElementById("projekt1");
+console.log(window);
+if (window.matchMedia("(min-width:700px)").matches) {
+    window.onload = () => {
+        var proj1 = document.getElementById("projekt1");
+        var homeText =  document.getElementById("homeText");
 
-var oldColor=document.body.style.background;
-proj1.addEventListener("mouseover", ()=>{document.body.style.backgroundImage = "url('bild2.jpg')";});
-proj1.addEventListener("mouseout", ()=>{document.body.style.background = oldColor;});
+        var oldbackColor = document.body.style.background;
+
+        proj1.addEventListener("mouseover", () => { 
+            proj1.style.color = "#242e37";
+            proj1.style.background = "yellow";
+            
+            console.log(homeText.children);
+            homeText.children[0].textContent="Projekt 1";
+            homeText.children[1].textContent="Stuff zum Projekt";
+           // proj1.innerText="holla"
+            document.body.style.backgroundImage = "url('bild2.jpg')"; });
+        proj1.addEventListener("mouseout", () => { 
+            proj1.style.background = oldbackColor;
+            document.body.style.background = oldColor; });
+    }
+}
+
+
+
+
