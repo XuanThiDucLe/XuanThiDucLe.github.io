2b03ee68d6af611e2e75a6265f9e026eb6c7305e
Xuan <xuan@Heidis-Air-2.fritz.box>
2019-06-01 22:11:23 +0200

    j

--- home.js
+++ home.js
@@ -2,82 +2,51 @@ console.log("hello world");
 
 console.log(window);
 
+var texte = [{ titel: "Projekt1", text: "Text zu Projekt 1" }, { titel: "Projekt2", text: "Text zu Projekt 2" }, { titel: "Projekt3", text: "Text zu Projekt 3" }]
 
 if (window.matchMedia("(min-width:700px)").matches) {
     window.onload = () => {
-
-        var proj1 = document.getElementById("projekt1");
-        var proj2 = document.getElementById("projekt2");
-        var proj3 = document.getElementById("projekt3");
-        var homeText = document.getElementById("homeText");
-        let notActive = "white";
-        var active = {border:"yellow", background:"yellow"};
-
-        var oldhomeText = homeText.cloneNode(true);
         
-        
-
-
-        var oldColor = document.body.style.background;
-        proj1.addEventListener("mouseover", () => {
-            proj1.style.borderColor = active.border;
-            proj1.style.background = active.background;
-            proj1.style.color = "#242e37";
-           
-            document.body.style.backgroundImage = "url('bild2.jpg')";
-            homeText.children[0].textContent = "Projekt 1";
-            homeText.children[1].textContent = "Stuff zum Projekt";
-            
-        });
-        
-        proj1.addEventListener("mouseout", () => {
-            homeText.children[0].textContent = oldhomeText.children[0].textContent;
-            homeText.children[1].textContent = oldhomeText.children[1].textContent;
-
-            proj1.style.background = oldColor;
-            proj1.style.borderColor = notActive;
-            proj1.style.color =notActive;
-
-            document.body.style.background = oldColor;
-            console.log(oldhomeText.children[0].textContent);
+        for (let i = 0; i < texte.length; i++) {
+            let ele = "projekt" + (i + 1);
+            let pic = "url('bild" + (i + 2) + ".jpg')";
+            let proj = document.getElementById(ele);
+            let homeText = document.getElementById("homeText");
+            let notActive = "white";
+            let active = { border: "yellow", background: "yellow" };
+            let oldhomeText = homeText.cloneNode(true);
+            console.log(ele);
+            console.log(texte[i].titel);
             
 
-        });
+            var oldColor = document.body.style.background;
 
-        proj2.addEventListener("mouseover", () => {
-            proj2.style.borderColor = active.border;
-            proj2.style.background = active.background;
-            proj2.style.color = "#242e37";
-          
-            homeText.children[0].textContent = "Projekt 2";
-            homeText.children[1].textContent = "Stuff zum Projekt zu stuff 2 ";
-            document.body.style.backgroundImage = "url('bild3.jpg')";
-        });
+            proj.addEventListener("mouseover", () => {
+                proj.style.borderColor = active.border;
+                proj.style.background = active.background;
+                proj.style.color = "#242e37";
+                console.log(pic)
+                document.body.style.backgroundImage = pic;
+                homeText.children[0].textContent = texte[i].titel;
+                homeText.children[1].textContent = texte[i].text;
 
-        proj2.addEventListener("mouseout", () => {
-            proj1.style.borderColor = notActive;
-            proj2.style.color = notActive;
-            proj2.style.background = oldColor;
-            
-            document.body.style.background = oldColor;
-        });
+            });
 
-        proj3.addEventListener("mouseover", () => {
-            proj3.style.borderColor = active.border;
-            proj3.style.background = active.background;
-            proj3.style.color = "#242e37";
-            
-            homeText.children[0].textContent = "Projekt 3";
-            homeText.children[1].textContent = "Stuff zum Projekt zu stuff 3 lalalala ";
-            document.body.style.backgroundImage = "url('bild4.jpg')";
-        });
+            proj.addEventListener("mouseout", () => {
+                homeText.children[0].textContent = oldhomeText.children[0].textContent;
+                homeText.children[1].textContent = oldhomeText.children[1].textContent;
+
+                proj.style.background = oldColor;
+                proj.style.borderColor = notActive;
+                proj.style.color = notActive;
+
+                document.body.style.background = oldColor;
+                //console.log(oldhomeText.children[0].textContent);
 
 
-        proj3.addEventListener("mouseout", () => {
-            proj3.style.background = oldColor;
-            proj3.style.color = "white";
-            document.body.style.background = oldColor;
-        });
+            });
+        }
+
     }
 
 }
