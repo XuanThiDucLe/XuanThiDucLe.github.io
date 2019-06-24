9dc91ee4f7c67e5437ad45e16fe61dbdc3c5944a
Xuan <xuan@Heidis-MacBook-Air-2.local>
2019-06-24 13:08:11 +0200

    test

--- project.js
+++ project.js
@@ -1,24 +1,6 @@
 if (window.matchMedia("(min-width:915px)").matches) {
   window.onload = () => {
-    console.log("stuff");
-
-    let mail = document.getElementById("mail");
-    let mfont = document.getElementById("mailfont");
-    let notActive = "white";
-    let active = { border: "#FFD74A", background: "#FFD74A" };
-    let oldColor = "#0F1217";
-
-    mail.addEventListener("mouseover", () => {
-        mail.style.borderColor = active.border;
-        mail.style.background = active.background;
-        mfont.style.color = oldColor;
-    });
-    mail.addEventListener("mouseout", () => {
-        mail.style.background = oldColor;
-        mail.style.borderColor = notActive;
-        mfont.style.color = notActive;
-    });
-
+   
     window.onscroll = function () { scrollFunction() };
     
     //navbar scroll 
