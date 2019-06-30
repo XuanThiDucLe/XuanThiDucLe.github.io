02d2dee86b77960f1218b3dcc6100ebb56ed5fbb
Xuan <xuan@Heidis-MacBook-Air-2.local>
2019-06-30 17:57:59 +0200

    animation

---
+++ test/project.js
@@ -0,0 +1,64 @@
+if (window.matchMedia("(min-width:915px)").matches) {
+  window.onload = () => {
+   
+    window.onscroll = function () { scrollFunction() };
+    
+    //navbar scroll 
+    function scrollFunction() {
+      if (document.body.scrollTop > 545 || document.documentElement.scrollTop > 545) {
+       // document.getElementsByClassName("fixed-top")[0].style.padding = "-30px";
+       document.getElementById("navbarNav").style.marginTop =  "0px";
+       document.getElementById("navbarNav2").style.marginTop =  "0px";
+       document.getElementById("logo").style.marginTop =  "-15px";
+       document.getElementById("logo").style.fontSize =  "26px";
+       document.getElementById("navbarCol").style.backgroundColor=  "#242E37";
+      
+       //unten
+      } else {
+      //  document.getElementsByClassName("fixed-top")[0].style.padding = "300px";
+        document.getElementById("navbarNav").style.marginTop =  "0px";
+        document.getElementById("navbarNav2").style.marginTop =  "-150px";
+        document.getElementById("logo").style.marginTop =  "0px";
+        document.getElementById("logo").style.fontSize =  "40px";
+        document.getElementById("navbarCol").style.backgroundColor=  "transparent";
+    
+        //oben 
+      }
+    }
+
+    
+
+  }
+}else{
+  window.onload = () => {
+    document.getElementsByClassName("navbar")[0].classList.remove("fixed-top");
+
+
+  }
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
+
+
+
+
+
+
+}
