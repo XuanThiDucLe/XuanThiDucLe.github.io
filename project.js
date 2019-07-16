bc551cc17907b472ac7f41cb365d470ef2e82bf3
XuLe <xuan@Heidis-MacBook-Air-2.local>
2019-07-16 17:48:43 +0200

    keine ahnung mehr

--- project.js
+++ project.js
@@ -11,7 +11,8 @@ if (window.matchMedia("(min-width:915px)").matches) {
        document.getElementById("navbarNav2").style.marginTop =  "0px";
        document.getElementById("logo").style.marginTop =  "-15px";
        document.getElementById("logo").style.fontSize =  "26px";
-       document.getElementById("navbarCol").style.backgroundColor=  "#0F1217";
+      //document.getElementById("navbarCol").style.backgroundColor=  "#0F1217";
+      document.getElementById("navbarCol").style.backgroundColor=  "rgba(15, 18, 23, 0.6)";
        document.getElementById("navbarCol").style.boxShadow= "0px 5px 20px black";
       
        //unten
