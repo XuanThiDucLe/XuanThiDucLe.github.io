4667eced01c2c140c549623426f5d26f8f12ea46
XuLe <xuan@Heidis-MacBook-Air-2.local>
2019-07-24 22:38:49 +0200

    1 und 2 sind nun fertig

--- project.js
+++ project.js
@@ -1,135 +1,148 @@
+
 //navbar
 
 if (window.matchMedia("(min-width:915px)").matches) {
   window.onload = () => {
 
     window.onscroll = function () { scrollFunction() };
-    
+
     //navbar scroll 
     function scrollFunction() {
       if (document.body.scrollTop > 545 || document.documentElement.scrollTop > 545) {
-       // document.getElementsByClassName("fixed-top")[0].style.padding = "-30px";
-       document.getElementById("navbarNav").style.marginTop =  "0px";
-       document.getElementById("navbarNav2").style.marginTop =  "0px";
-       document.getElementById("logo").style.marginTop =  "-15px";
-       document.getElementById("logo").style.fontSize =  "26px";
-      //document.getElementById("navbarCol").style.backgroundColor=  "#0F1217";
-     // document.getElementById("navbarCol").style.backgroundColor=  "rgba(15, 18, 23, 0.6)";
-      document.getElementById("navbarCol").style.backgroundColor=  "rgba(15, 18, 23, 1)";
+        // document.getElementsByClassName("fixed-top")[0].style.padding = "-30px";
+        document.getElementById("navbarNav").style.marginTop = "0px";
+        document.getElementById("navbarNav2").style.marginTop = "0px";
+        document.getElementById("logo").style.marginTop = "-15px";
+        document.getElementById("logo").style.fontSize = "26px";
+        //document.getElementById("navbarCol").style.backgroundColor=  "#0F1217";
+        // document.getElementById("navbarCol").style.backgroundColor=  "rgba(15, 18, 23, 0.6)";
+        document.getElementById("navbarCol").style.backgroundColor = "rgba(15, 18, 23, 1)";
 
-      document.getElementById("navbarCol").style.boxShadow= "0px 5px 20px black";
-      
-       //unten
+        document.getElementById("navbarCol").style.boxShadow = "0px 5px 20px black";
+
+        //unten
       } else {
-      //  document.getElementsByClassName("fixed-top")[0].style.padding = "300px";
-        document.getElementById("navbarNav").style.marginTop =  "0px";
-        document.getElementById("navbarNav2").style.marginTop =  "-150px";
-        document.getElementById("logo").style.marginTop =  "0px";
-        document.getElementById("logo").style.fontSize =  "40px";
-        document.getElementById("navbarCol").style.backgroundColor=  "transparent";
-        document.getElementById("navbarCol").style.boxShadow=  "none";
-    
+        //  document.getElementsByClassName("fixed-top")[0].style.padding = "300px";
+        document.getElementById("navbarNav").style.marginTop = "0px";
+        document.getElementById("navbarNav2").style.marginTop = "-150px";
+        document.getElementById("logo").style.marginTop = "0px";
+        document.getElementById("logo").style.fontSize = "40px";
+        document.getElementById("navbarCol").style.backgroundColor = "transparent";
+        document.getElementById("navbarCol").style.boxShadow = "none";
+
         //oben 
       }
     }
 
-        //___ active button 
+        //videohover
 
-        let mail = document.getElementById("mail");
-        let mfont = document.getElementById("mailfont");
-        let notActive = "white";
-        let active = { border: "#FFD74A", background: "#FFD74A" };
-        let oldColor = "#0F1217";
+        var figure = $(".video").hover(hoverVideo, hideVideo);
+
+        function hoverVideo(e) {
+          $('video', this).get(0).play();
+        }
     
-        mail.addEventListener("mouseover", () => {
-            mail.style.borderColor = active.border;
-            mail.style.background = active.background;
-            mfont.style.color = oldColor;
-        });
-        mail.addEventListener("mouseout", () => {
-            mail.style.background = oldColor;
-            mail.style.borderColor = notActive;
-            mfont.style.color = notActive;
-        });
+        function hideVideo(e) {
+          $('video', this).get(0).pause();
+        }
+    
 
-    //___active button end 
+    //___ active button 
+
+    let mail = document.getElementById("mail");
+    let mfont = document.getElementById("mailfont");
+    let notActive = "white";
+    let active = { border: "#FFD74A", background: "#FFD74A" };
+    let oldColor = "#0F1217";
+
+    mail.addEventListener("mouseover", () => {
+      mail.style.borderColor = active.border;
+      mail.style.background = active.background;
+      mfont.style.color = oldColor;
+    });
+    mail.addEventListener("mouseout", () => {
+      mail.style.background = oldColor;
+      mail.style.borderColor = notActive;
+      mfont.style.color = notActive;
+    });
+ //___active button end 
 
   }
-}else{
+} else {
   window.onload = () => {
     document.getElementsByClassName("navbar")[0].classList.remove("fixed-top");
 
 
   }
 
-window.onscroll = function() {scrollFunction()};
+  window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("myBtn").style.display = "block";
-  } else {
-    document.getElementById("myBtn").style.display = "none";
+  function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
   }
-}
 
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+  function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  }
 
 }
 
 //scroll spy 
 
-$(document).ready(function(){
-    
-  var getMax = function(){
-      return $(document).height() - $(window).height();
-  }
-  
-  var getValue = function(){
-      return $(window).scrollTop();
-  }
-  
-  if('max' in document.createElement('progress')){
-      // Browser supports progress element
-      var progressBar = $('progress');
-      
-      // Set the Max attr for the first time
-      progressBar.attr({ max: getMax() });
+$(document).ready(function () {
 
-      $(document).on('scroll', function(){
-          // On scroll only Value attr needs to be calculated
-          progressBar.attr({ value: getValue() });
-      });
-    
-      $(window).resize(function(){
-          // On resize, both Max/Value attr needs to be calculated
-          progressBar.attr({ max: getMax(), value: getValue() });
-      });   
+  var getMax = function () {
+    return $(document).height() - $(window).height();
+  }
+
+  var getValue = function () {
+    return $(window).scrollTop();
+  }
+
+  if ('max' in document.createElement('progress')) {
+    // Browser supports progress element
+    var progressBar = $('progress');
+
+    // Set the Max attr for the first time
+    progressBar.attr({ max: getMax() });
+
+    $(document).on('scroll', function () {
+      // On scroll only Value attr needs to be calculated
+      progressBar.attr({ value: getValue() });
+    });
+
+    $(window).resize(function () {
+      // On resize, both Max/Value attr needs to be calculated
+      progressBar.attr({ max: getMax(), value: getValue() });
+    });
   }
   else {
-      var progressBar = $('.progress-bar'), 
-          max = getMax(), 
-          value, width;
-      
-      var getWidth = function(){
-          // Calculate width in percentage
-          value = getValue();            
-          width = (value/max) * 100;
-          width = width + '%';
-          return width;
-      }
-      
-      var setWidth = function(){
-          progressBar.css({ width: getWidth() });
-      }
-      
-      $(document).on('scroll', setWidth);
-      $(window).on('resize', function(){
-          // Need to reset the Max attr
-          max = getMax();
-          setWidth();
-      });
+    var progressBar = $('.progress-bar'),
+      max = getMax(),
+      value, width;
+
+    var getWidth = function () {
+      // Calculate width in percentage
+      value = getValue();
+      width = (value / max) * 100;
+      width = width + '%';
+      return width;
+    }
+
+    var setWidth = function () {
+      progressBar.css({ width: getWidth() });
+    }
+
+    $(document).on('scroll', setWidth);
+    $(window).on('resize', function () {
+      // Need to reset the Max attr
+      max = getMax();
+      setWidth();
+    });
   }
 });
